4f1fddd46fc9497361d443d8b99adb337973ee6f
Yannick Kuhar <yannick.kuh@gmail.com>
2019-08-03 17:25:41 +0200

    Basic main menu done. # TODO: Swap buttons with images.

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void LoadLevel()
+	{
+		Debug.Log("yes0");
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitGame()
+	{
+		Debug.Log("yes1");
+		Application.Quit();
+	}
+}
